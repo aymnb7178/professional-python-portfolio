ff68be4f0d041ca311b7b05bdbc88ed441e1df39
aymnb7178 <aymnb7178@gmail.com>
2026-05-10 11:36:45 +0300

    Add sample code for fitness project

---
+++ projects/fitness/fitness_calculator.py
@@ -0,0 +1,7 @@
+# Fitness Calculator - Example Code
+
+def calculate_bmi(weight, height):
+    return round(weight / ((height / 100) ** 2), 2)
+
+print('BMI Calculator Ready!')
+# Add more features as needed
